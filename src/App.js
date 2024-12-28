7696136e7dee1d2f4f3bc887762640b41c0f58d3
dsad233 <ksl1421@naver.com>
2024-12-28 19:14:34 +0900

    Fix : 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 
-let postTitle = '제목문';
-let body = '바디값';
-let botton = "여기는 하단 바 입니다.";
-
 function App() {
+  // let postTitle = '제목문';
+  let botton = "여기는 하단 바 입니다.";
+
+  const nowDate = String(new Date());
+
+  const [log, func] = useState("블로그");
   return (
     <div className="App">
       <div className='loginbar'>
@@ -15,13 +18,48 @@ function App() {
       </div>
       <header>
       <div className='titlebox'>
-        <h4 className='title'>{ postTitle }</h4>
+        <h4 className='title'>{ log }</h4>
       </div>
       </header>
 
       <body>
         <div className='bodybox'>
-          <p className='bodyp'>{ body }</p>
+          {/* <p className='bodyp'>{ body }</p> */}
+          <div className='ptagbox'>
+            <p>게시글 제목</p>
+            <p>게시글 내용</p>
+            <p>{ nowDate }</p>
+          </div>
+          <div className='ptagbox'>
+            <p>게시글 제목</p>
+            <p>게시글 내용</p>
+            <p>{ nowDate }</p>
+          </div>
+          <div className='ptagbox'>
+            <p>게시글 제목</p>
+            <p>게시글 내용</p>
+            <p>{ nowDate }</p>
+          </div>
+          <div className='ptagbox'>
+            <p>게시글 제목</p>
+            <p>게시글 내용</p>
+            <p>{ nowDate }</p>
+          </div>
+          <div className='ptagbox'>
+            <p>게시글 제목</p>
+            <p>게시글 내용</p>
+            <p>{ nowDate }</p>
+          </div>
+          <div className='ptagbox'>
+            <p>게시글 제목</p>
+            <p>게시글 내용</p>
+            <p>{ nowDate }</p>
+          </div>
+          <div className='ptagbox'>
+            <p>게시글 제목</p>
+            <p>게시글 내용</p>
+            <p>{ nowDate }</p>
+          </div>
         </div>
       </body>
 
